8fe50591531b75a7b815755e86c087fb34eb3d46
Mauro Villagra <maurovillagra257@gmail.com>
2023-07-23 19:59:51 -0300

    Subi el punto 6

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CardColores from './componentes/CardColores'
+import FormularioColores from './componentes/FormularioColores'
+function App() {
+
+  return (
+    <>
+    <FormularioColores/>
+
+    </>
+  )
+}
+
+export default App
